29e2332e79497f995b9e0c84f0093ff70d49fb36
Yahweasel <git-rpo@yahweasel.com>
2024-03-28 20:21:32 -0400

    Generalizations and perfecting techniques

--- motion-transfer.c
+++ motion-transfer.c
@@ -16,6 +16,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
@@ -26,6 +27,18 @@
 #define DECODER "h263p"
 
 static char avError[1024] = {0};
+
+struct List {
+    struct List *next;
+    void *val;
+};
+
+enum ArgMode {
+    ARG_NONE,
+    ARG_MOTION,
+    ARG_INPUT,
+    ARG_OUTPUT
+};
 
 #define SF0(ret, func, args) do { \
     (ret) = func args; \
@@ -44,15 +57,14 @@ static char avError[1024] = {0};
     } \
 } while (0)
 
-static AVFrame *scaleFrame(
-    AVFrame *inFrame, double factor, enum AVPixelFormat toPixFmt
-) {
+static AVFrame *scaleFrame(AVFrame *inFrame, enum AVPixelFormat toPixFmt)
+{
     struct SwsContext *swsc;
     AVFrame *outFrame;
     int ret;
 
     SFN(swsc, sws_getContext, (inFrame->width, inFrame->height, inFrame->format, 
-                               inFrame->width * factor, inFrame->height * factor, toPixFmt,
+                               inFrame->width, inFrame->height, toPixFmt,
                                0, NULL, NULL, NULL));
     SFN(outFrame, av_frame_alloc, ());
     ret = sws_scale_frame(swsc, outFrame, inFrame);
@@ -64,7 +76,7 @@ static AVFrame *scaleFrame(
     return outFrame;
 }
 
-static AVFrame *inputFrame(char *inUrl)
+static AVFrame *readFrame(char *inUrl)
 {
     AVFormatContext *fc = NULL;
     AVCodecContext *cc;
@@ -94,7 +106,7 @@ static AVFrame *inputFrame(char *inUrl)
     avcodec_free_context(&cc);
     avformat_close_input(&fc);
 
-    yuvFrame = scaleFrame(frame, 1, AV_PIX_FMT_YUV420P);
+    yuvFrame = scaleFrame(frame, AV_PIX_FMT_YUV420P);
     av_frame_free(&frame);
 
     return yuvFrame;
@@ -117,6 +129,7 @@ static AVCodecContext *getEncoder(AVFrame *frame)
     cc->flags |= AV_CODEC_FLAG_QSCALE;
     cc->global_quality = 1;
     cc->bit_rate = frame->height * 100000LL;
+    cc->keyint_min = cc->gop_size = 600;
     opts = NULL;
     SF0(ret, av_dict_set, (&opts, "intra_penalty", "256", 0));
     SF0(ret, av_dict_set, (&opts, "crf", "23", 0));
@@ -126,48 +139,78 @@ static AVCodecContext *getEncoder(AVFrame *frame)
     return cc;
 }
 
-int main(int argc, char **argv)
+void readMotionFile(
+    char *file, AVCodecContext **cc, struct List **head, struct List **tail
+) {
+    AVFrame *frame;
+    AVPacket *pkt;
+    struct List *lst;
+    int ret;
+
+    if (file) {
+        frame = readFrame(file);
+        frame->pict_type = AV_PICTURE_TYPE_NONE;
+    } else {
+        frame = NULL;
+    }
+
+    if (*cc) {
+        /* The codec is available, so encode this motion */
+        SF0(ret, avcodec_send_frame, (*cc, frame));
+        while (1) {
+            SFN(pkt, av_packet_alloc, ());
+            ret = avcodec_receive_packet(*cc, pkt);
+            if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                break;
+            if (ret < 0)
+                SF0(ret, avcodec_receive_packet, (*cc, pkt));
+            SFN(lst, (struct List *) calloc, (1, sizeof(struct List)));
+            lst->val = pkt;
+            if (*tail) {
+                (*tail)->next = lst;
+                *tail = lst;
+            } else {
+                *head = *tail = lst;
+            }
+            pkt = NULL;
+        }
+
+    } else {
+        /* The codec hasn't been allocated, so just send the initial data */
+        SFN(pkt, av_packet_alloc, ());
+        *cc = getEncoder(frame);
+        SF0(ret, avcodec_send_frame, (*cc, frame));
+        while (ret == 0) {
+            av_packet_unref(pkt);
+            ret = avcodec_receive_packet(*cc, pkt);
+        }
+        av_packet_free(&pkt);
+    }
+
+    av_frame_free(&frame);
+
+    if (!file) {
+        /* Clean up the encoder */
+        avcodec_free_context(cc);
+    }
+}
+
+AVFrame *applyMotion(AVFrame *firstFrame, struct List *motion)
 {
-    AVFrame *i1a, *i1b, *i2a, *o2bYUV, *o2b;
-    const AVCodec *c;
-    AVFormatContext *pngFmt;
     AVCodecContext *ecc, *dcc;
-    AVPacket *iframePkt, *motionPkt, *pngPkt;
-    AVStream *pngStr;
-    const AVPixFmtDescriptor *pixDesc;
-    AVDictionary *opts;
-    int ret, ret2;
+    const AVCodec *c;
+    AVPacket *iframePkt;
+    AVFrame *outputFrame;
+    int ret;
 
-    /* Read input frames */
-    i1a = inputFrame(argv[1]);
-    i1b = inputFrame(argv[2]);
-    i2a = inputFrame(argv[3]);
-
-    /* Encode the motion */
-    ecc = getEncoder(i1a);
-    i1a->pts = 0;
-    i1a->pict_type = AV_PICTURE_TYPE_NONE;
-    SF0(ret, avcodec_send_frame, (ecc, i1a));
-    i1b->pts = 1;
-    i1b->pict_type = AV_PICTURE_TYPE_NONE;
-    SF0(ret, avcodec_send_frame, (ecc, i1b));
     SFN(iframePkt, av_packet_alloc, ());
-    ret = avcodec_receive_packet(ecc, iframePkt);
-    av_packet_unref(iframePkt);
-    SFN(motionPkt, av_packet_alloc, ());
-    if (ret < 0) {
-        SF0(ret, avcodec_send_frame, (ecc, NULL));
-        SF0(ret, avcodec_receive_packet, (ecc, iframePkt));
-    }
-    SF0(ret, avcodec_receive_packet, (ecc, motionPkt));
-    avcodec_free_context(&ecc);
+    SFN(outputFrame, av_frame_alloc, ());
 
-    /* Encode the second non-motion */
-    ecc = getEncoder(i2a);
-    i2a->pts = 0;
-    i2a->pict_type = AV_PICTURE_TYPE_NONE;
-    SF0(ret, avcodec_send_frame, (ecc, i2a));
-    av_packet_unref(iframePkt);
+    /* Make the I-frame packet */
+    ecc = getEncoder(firstFrame);
+    firstFrame->pts = 0;
+    firstFrame->pict_type = AV_PICTURE_TYPE_NONE;
+    SF0(ret, avcodec_send_frame, (ecc, firstFrame));
     ret = avcodec_receive_packet(ecc, iframePkt);
     if (ret < 0) {
         SF0(ret, avcodec_send_frame, (ecc, NULL));
@@ -175,39 +218,59 @@ int main(int argc, char **argv)
     }
     avcodec_free_context(&ecc);
 
-    /* Apply the 1 motion to the 2 frame */
+    /* Decode using this I-frame and those motion packets */
     SFN(c, avcodec_find_decoder_by_name, (DECODER));
     SFN(dcc, avcodec_alloc_context3, (c));
     SF0(ret, avcodec_open2, (dcc, c, NULL));
     SF0(ret, avcodec_send_packet, (dcc, iframePkt));
-    SFN(o2bYUV, av_frame_alloc, ());
-    ret2 = avcodec_receive_frame(dcc, o2bYUV);
-    SF0(ret, avcodec_send_packet, (dcc, motionPkt));
-    SF0(ret, avcodec_send_packet, (dcc, NULL));
-    if (ret2 < 0)
-        SF0(ret, avcodec_receive_frame, (dcc, o2bYUV));
-    av_frame_unref(o2bYUV);
-    SF0(ret, avcodec_receive_frame, (dcc, o2bYUV));
+    av_packet_free(&iframePkt);
+    for (; motion; motion = motion->next) {
+        while (1) {
+            av_frame_unref(outputFrame);
+            if (avcodec_receive_frame(dcc, outputFrame) >= 0)
+                break;
+        }
+        SF0(ret, avcodec_send_packet, (dcc, (AVPacket *) motion->val));
+    }
+    if (avcodec_receive_frame(dcc, outputFrame) < 0) {
+        SF0(ret, avcodec_send_packet, (dcc, NULL));
+        SF0(ret, avcodec_receive_frame, (dcc, outputFrame));
+    }
+    avcodec_free_context(&dcc);
 
-    o2b = scaleFrame(o2bYUV, 1, AV_PIX_FMT_RGB24);
-    av_frame_free(&o2bYUV);
+    return outputFrame;
+}
+
+void writeFrame(char *file, AVFrame *yuvFrame)
+{
+    AVFrame *rgbFrame;
+    AVPacket *pngPkt;
+    const AVCodec *c;
+    AVCodecContext *ecc;
+    AVFormatContext *pngFmt = NULL;
+    AVStream *pngStr;
+    AVDictionary *opts;
+    int ret;
+
+    rgbFrame = scaleFrame(yuvFrame, AV_PIX_FMT_RGB24);
 
     /* Encode it as png */
     SFN(c, avcodec_find_encoder_by_name, ("png"));
     SFN(ecc, avcodec_alloc_context3, (c));
     ecc->time_base.num = 1;
     ecc->time_base.den = 60;
-    ecc->width = o2b->width;
-    ecc->height = o2b->height;
+    ecc->width = rgbFrame->width;
+    ecc->height = rgbFrame->height;
     ecc->pix_fmt = AV_PIX_FMT_RGB24;
     SF0(ret, avcodec_open2, (ecc, c, NULL));
-    SF0(ret, avcodec_send_frame, (ecc, o2b));
+    SF0(ret, avcodec_send_frame, (ecc, rgbFrame));
+    av_frame_free(&rgbFrame);
     SFN(pngPkt, av_packet_alloc, ());
     SF0(ret, avcodec_receive_packet, (ecc, pngPkt));
     pngPkt->pts = 1;
 
     /* And save it as png */
-    SF0(ret, avformat_alloc_output_context2, (&pngFmt, NULL, "image2", argv[4]));
+    SF0(ret, avformat_alloc_output_context2, (&pngFmt, NULL, "image2", file));
     SFN(pngStr, avformat_new_stream, (pngFmt, NULL));
     SF0(ret, avcodec_parameters_from_context, (pngStr->codecpar, ecc));
     opts = NULL;
@@ -216,6 +279,81 @@ int main(int argc, char **argv)
     av_dict_free(&opts);
     SF0(ret, av_write_frame, (pngFmt, pngPkt));
     SF0(ret, av_write_trailer, (pngFmt));
+    avformat_free_context(pngFmt);
+}
+
+int main(int argc, char **argv)
+{
+    enum ArgMode argMode = ARG_NONE;
+    int argi;
+
+    struct List *motionList = NULL, *motionListTail = NULL;
+    AVFrame *inputFrame = NULL;
+    AVCodecContext *motionCCtx = NULL;
+
+    for (argi = 1; argi < argc; argi++) {
+        char *arg = argv[argi];
+
+        if (arg[0] == '-') {
+            if (motionCCtx)
+                readMotionFile(NULL, &motionCCtx, &motionList, &motionListTail);
+
+            if (!strcmp(arg, "-m")) {
+                struct List *lst, *next;
+
+                /* Read in motion data */
+                argMode = ARG_MOTION;
+
+                /* Free any existing data */
+                for (lst = motionList; lst; lst = next) {
+                    next = lst->next;
+                    av_packet_free((AVPacket **) &lst->val);
+                    free(lst);
+                }
+
+            } else if (!strcmp(arg, "-i")) {
+                argMode = ARG_INPUT;
+
+            } else if (!strcmp(arg, "-o")) {
+                argMode = ARG_OUTPUT;
+
+            } else {
+                fprintf(stderr, "Unrecognized argument: %s\n", arg);
+                exit(1);
+
+            }
+
+            continue;
+        }
+
+        /* Filename */
+        switch (argMode) {
+            case ARG_MOTION:
+                readMotionFile(arg, &motionCCtx, &motionList, &motionListTail);
+                break;
+
+            case ARG_INPUT:
+                argMode = ARG_NONE;
+                if (inputFrame)
+                    av_frame_free(&inputFrame);
+                inputFrame = readFrame(arg);
+                break;
+
+            case ARG_OUTPUT:
+            {
+                AVFrame *outputFrame;
+                argMode = ARG_NONE;
+                outputFrame = applyMotion(inputFrame, motionList);
+                writeFrame(arg, outputFrame);
+                av_frame_free(&outputFrame);
+                break;
+            }
+
+            default:
+                fprintf(stderr, "Unexpected argument: %s\n", arg);
+                exit(1);
+        }
+    }
 
     return 0;
 }
